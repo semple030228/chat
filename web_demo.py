04608cb472c624c6f66d16e593ff5cd5bc8637ed
semple030228 <62066626+semple030228@users.noreply.github.com>
2024-01-11 21:00:26 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -24,7 +24,7 @@ def init():
 def load_chain():
     # 加载问答链
     # 定义 Embeddings
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformer")
+    embeddings = HuggingFaceEmbeddings(model_name="Shanghai_AI_Laboratory/sentence-transformer")
 
     # 向量数据库持久化路径
     persist_directory = 'data_base/vector_db/chroma'
